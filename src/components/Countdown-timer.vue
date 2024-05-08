6d6e5ba41f78547f3c25dc4b7b86c67ae3cc2c01
DanielPincu <ghepin01@easv365.dk>
2024-05-08 17:26:30 +0200

    Button added

---
+++ src/components/Countdown-timer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="text-center text-2xl text-orange-500">
+      <p>Time Left: {{ days }} days, {{ hours }} hours, {{ minutes }} minutes</p>
+    </div>
+  </template>
+  
+  <script setup>
+import { ref, computed, onMounted } from 'vue';
+
+const eventDate = new Date('2024-09-14T00:00:00');
+const currentDate = ref(new Date());
+
+const days = computed(() => {
+  const diffTime = eventDate.getTime() - currentDate.value.getTime();
+  return Math.floor(diffTime / (1000 * 60 * 60 * 24));
+});
+
+const hours = computed(() => {
+  const diffTime = eventDate.getTime() - currentDate.value.getTime();
+  return Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+});
+
+const minutes = computed(() => {
+  const diffTime = eventDate.getTime() - currentDate.value.getTime();
+  return Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+});
+
+onMounted(() => {
+  // Update current date every second
+  setInterval(() => {
+    currentDate.value = new Date();
+  }, 1000);
+});
+</script>
+
+  
--- src/components/Hero-section.vue
+++ src/components/Hero-section.vue
@@ -1,13 +1,18 @@
+
 <template>
   <div class="w-full relative">
     <video autoplay muted loop playsinline class="w-full">
       <source src="../assets/img/website-banner.mp4" type="video/mp4">
     </video>
     <div class="absolute top-1/4 left-0 right-0">
+
       <div class="bg-black bg-opacity-50">
+
         <div class="max-w-screen-xl mx-auto px-8 py-4">
           <h1 class="text-orange-400 text-center tracking-widest text-md xl:text-[40px] font-bold">14 September 2024</h1>
-         <h2 class="text-orange-400 pt-10 text-center tracking-widest text-md xl:text-[60px] font-bold">Fantasy Quest Esbjerg</h2>
+          <CountdownTimer/>
+          <BuyButton/>
+          <h2 class="text-orange-400 pt-10 text-center tracking-widest text-md xl:text-[60px] font-bold">Fantasy Quest Esbjerg</h2>
         </div>
         
 
@@ -17,9 +22,14 @@
               <img class="w-32 md:w-64 xl:w-96" src="../assets/img/drage.png" alt="">
             </div>
           </div>
-    </div>
-        
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import CountdownTimer from '../components/Countdown-timer.vue'
+import BuyButton from '../components/Buy-button.vue'
+
+</script>
