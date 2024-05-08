5cd0e86d034fd3e4d7955993540943aa042f48fc
DanielPincu <ghepin01@easv365.dk>
2024-05-08 16:58:48 +0200

    Updated

--- src/components/Hero-section.vue
+++ src/components/Hero-section.vue
@@ -6,7 +6,8 @@
     <div class="absolute top-1/4 left-0 right-0">
       <div class="bg-black bg-opacity-50">
         <div class="max-w-screen-xl mx-auto px-8 py-4">
-         <h1 class="text-orange-400 pt-10 text-center tracking-widest text-md xl:text-[60px] font-bold">Fantasy Quest Esbjerg</h1>
+          <h1 class="text-orange-400 text-center tracking-widest text-md xl:text-[40px] font-bold">14 September 2024</h1>
+         <h2 class="text-orange-400 pt-10 text-center tracking-widest text-md xl:text-[60px] font-bold">Fantasy Quest Esbjerg</h2>
         </div>
         
 
@@ -20,12 +21,5 @@
         
       </div>
     </div>
-    <!-- <div class="absolute top-0 left-0 right-0 transform -translate-y-1/8">
-      <div>
-        <div class="max-w-screen-xl mx-auto px-8 py-4">
-          <img class="w-32 md:w-64 xl:w-96" src="../assets/img/drage.png" alt="">
-        </div>
-      </div>
-    </div> -->
   </div>
 </template>
