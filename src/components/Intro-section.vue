8862a972af4d58cbff51b7b4b218e5e6f33b6d3b
DanielPincu <ghepin01@easv365.dk>
2024-05-09 11:45:51 +0200

    Updated

--- src/components/Intro-section.vue
+++ src/components/Intro-section.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="text-3xl text-justify tracking-tighter flex flex-col items-center md:pt-5 pt-40 justify-center mx-[1em] md:mx-24">
+    <div class="text-3xl italic text-justify tracking-tighter flex flex-col items-center md:pt-5 pt-44 justify-center mx-[1em] md:mx-24">
         <p>Imagine "Prisoners at the Fort" with a lot of magic, some theatre and role playing, some heroes and villains and of course the battle between good and evil, then you are well on your way to Fantasy Quest Esbjerg.</p>
         <br>
-        <p>In 2024, Fantasy Quest Esbjerg will have both a difficult evening edition and a family-friendly edition during the day during the Fantasy Festival, written in collaboration with the YouTuber Lakserytteren, where everyone can participate.</p>
+        <p>In 2024, Fantasy Quest Esbjerg will have both a <span class="text-orange-500 text-4xl">difficult evening</span> edition and a <span class="text-orange-500 text-4xl">family-friendly</span> edition during the day during the Fantasy Festival, written in collaboration with the YouTuber Lakserytteren, where everyone can participate.</p>
 
-        <h1 class="text-[1em] text-center  md:text-[2em] py-20">Fantasy Quest Esbjerg</h1>
+        <h1 class="text-center text-[64px] pt-24 pb-10">Fantasy Quest Esbjerg</h1>
+        <p class="pb-20">Are you ready to live into another world, a magical story where you are the hero who is on a "quest" to overcome the evil forces and save the whole world? So get ready for Fantasy Quest Esbjerg.</p>
     </div>
 </template>
 
