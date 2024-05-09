e2313700e98cab1ccde34675dc82f9be394d7169
DanielPincu <ghepin01@easv365.dk>
2024-05-09 12:12:34 +0200

    updated

--- src/components/Intro-section.vue
+++ src/components/Intro-section.vue
@@ -1,11 +1,21 @@
 <template>
-    <div class="text-3xl italic text-justify tracking-tighter flex flex-col items-center md:pt-5 pt-44 justify-center mx-[1em] md:mx-24">
-        <p>Imagine "Prisoners at the Fort" with a lot of magic, some theatre and role playing, some heroes and villains and of course the battle between good and evil, then you are well on your way to Fantasy Quest Esbjerg.</p>
+    <div class="text-3xl italic text-justify tracking-tighter flex flex-col items-center md:pt-5 pt-44 justify-center container mx-auto">
+        <p class="mx-5">Imagine "Prisoners at the Fort" with a lot of magic, some theatre and role playing, some heroes and villains and of course the battle between good and evil, then you are well on your way to Fantasy Quest Esbjerg.</p>
         <br>
-        <p>In 2024, Fantasy Quest Esbjerg will have both a <span class="text-orange-500 text-4xl">difficult evening</span> edition and a <span class="text-orange-500 text-4xl">family-friendly</span> edition during the day during the Fantasy Festival, written in collaboration with the YouTuber Lakserytteren, where everyone can participate.</p>
+        <p class="mx-5">In 2024, Fantasy Quest Esbjerg will have both a <span class="text-orange-500 text-4xl">difficult evening</span> edition and a <span class="text-orange-500 text-4xl">family-friendly</span> edition during the day during the Fantasy Festival, written in collaboration with the YouTuber Lakserytteren, where everyone can participate.</p>
 
-        <h1 class="text-center text-[64px] pt-24 pb-10">Fantasy Quest Esbjerg</h1>
-        <p class="pb-20">Are you ready to live into another world, a magical story where you are the hero who is on a "quest" to overcome the evil forces and save the whole world? So get ready for Fantasy Quest Esbjerg.</p>
+        <h1 class="text-center text-[60px] pt-24 pb-10">Fantasy Quest Esbjerg</h1>
+        <p class="pb-20 mx-5">Are you ready to live into another world, a magical story where you are the hero who is on a "quest" to overcome the evil forces and save the whole world? So get ready for Fantasy Quest Esbjerg.</p>
     </div>
+
+   
+<div class="grid grid-cols-2 grid-rows-2 container mx-auto">
+    <div class="hover:bg-orange-500 border-2 hover:border-4 border-orange-500 hover:border-black duration-100 h-10 mx-5 flex justify-center items-center">Family Quest</div>
+    <div class="hover:bg-orange-500 border-2 hover:border-4 border-orange-500 hover:border-black duration-100 h-10 mx-5 flex justify-center items-center">Night Quest</div>
+</div>
+
+
+
+
 </template>
 
