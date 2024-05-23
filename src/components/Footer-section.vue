9b152eaf61546caa5027c1b4e8df90042f4d1535
DanielPincu <ghepin01@easv365.dk>
2024-05-23 14:13:58 +0200

    Footer added

--- src/components/Footer-section.vue
+++ src/components/Footer-section.vue
@@ -1,6 +1,9 @@
 <template>
-     <footer class="w-full bg-gray-200 flex items-center justify-center text-center p-4">
-      <p>&copy; 2021 Vue 3 Router</p>
+     <footer class="w-full bg-[#1A202C] flex flex-col items-center justify-center text-center">
+      <img src="../assets/img/footer.png" alt="">
+      <div class="bg-[#165678] w-full flex justify-center">
+        <img src="../assets/img/sponsors.png" alt="">
+      </div>
     </footer>
 </template>
 
