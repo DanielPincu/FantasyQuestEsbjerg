e29308645d807c78207213369c34ea1bc584bdc3
DanielPincu <ghepin01@easv365.dk>
2024-05-23 11:09:34 +0200

    Countdown updated

--- src/components/Countdown-timer.vue
+++ src/components/Countdown-timer.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="text-center flex items-center justify-center" data-aos="fade-up">
-    <p class="md:text-3xl text-xl  text-gradient font-bold pr-5">TIME LEFT</p>
+  <div class="text-center flex items-center pt-5 justify-center" data-aos="fade-up">
+    <p class="md:text-3xl text-xl text-gradient pt-5 font-bold pr-5">TIME LEFT</p>
     <div class="flex justify-center items-center space-x-4">
-      <div class="w-14 h-14 border-4 border-orange-400 bg-orange-500 bg-opacity-20 text-slate-300 md:w-24 md:h-24 rounded-full flex items-center justify-center">
+      <div class="w-14 h-14 border-4 border-orange-400 bg-orange-500 bg-opacity-20 text-slate-300 md:w-24 md:h-24  arrow-up rounded-full flex items-center justify-center">
         <p class="text-white text-sm xl:text-2xl">{{ days }} days</p>
       </div>
       <p class="animate-pulse text-[3em]">:</p>
-      <div class="w-14 h-14 md:w-24 md:h-24 rounded-full border-4 border-orange-400 bg-orange-500 bg-opacity-20 flex items-center justify-center">
+      <div class="w-14 h-14 md:w-24 md:h-24 arrow-up rounded-full border-4 border-orange-400 bg-orange-500 bg-opacity-20 flex items-center justify-center">
         <p class="text-white text-sm xl:text-xl">{{ hours }} <br> hours</p>
       </div>
     </div>
@@ -47,4 +47,21 @@ onMounted(() => {
 
 </script>
 
-  
+<style>
+
+.arrow-up {
+  width: 100px; 
+  height: 100px; 
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-bottom: 4px solid orange;
+}
+
+@media only screen and (max-width: 767px) {
+  .arrow-up {
+    width: 50px;
+    height: 50px;
+  }
+}
+
+</style>
