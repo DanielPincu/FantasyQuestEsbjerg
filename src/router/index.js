39e02eac84ead72ab53832d9cfa48dd85847f2b4
DanielPincu <ghepin01@easv365.dk>
2024-05-23 14:39:10 +0200

    ROUTER Scroll TOP OK

--- src/router/index.js
+++ src/router/index.js
@@ -1,56 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: {
+      title: "Home"
+    }
+  },
+  {
+    path: '/family-quest',
+    name: 'Family Quest',
+    component: () => import('../views/FamilyView.vue'),
+    meta: {
+      title: "Family Quest"
+    }
+  },
+  {
+    path: '/night-quest',
+    name: 'Night Quest',
+    component: () => import('../views/NightView.vue'),
+    meta: {
+      title: "Night Quest"
+    }
+  },
+  {
+    path: '/faq',
+    name: 'FAQ',
+    component: () => import('../views/FAQView.vue'),
+    meta: {
+      title: "FAQ"
+    }
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      meta: {
-        title: "Home"
-      }
-    },
-    {
-      path: '/family-quest',
-      name: 'Family Quest',
-      component: () => import('../views/FamilyView.vue'),
-      meta: {
-        title: "Family Quest"
-      }
-    },
-
-    {
-      path: '/night-quest',
-      name: 'Night Quest',
-      component: () => import('../views/NightView.vue'),
-      meta: {
-        title: "Night Quest"
-      }
-    },
-
-    {
-      path: '/faq',
-      name: 'FAQ',
-      component: () => import('../views/FAQView.vue'),
-      meta: {
-        title: "FAQ"
-      }
-    },
-    
-  ]
-
-  
-})
+  routes,
+  scrollBehavior() {
+    // always scroll to top
+    return { top: 0 };
+  }
+});
 
 router.beforeEach((to, from, next) => {
   const pageTitle = to.meta.title || (to.meta.dynamicTitle && to.params.title); 
   document.title = `Fantasy Quest | ${pageTitle}`;
-  next()
-})
+  next();
+});
 
-
-
-export default router
+export default router;
