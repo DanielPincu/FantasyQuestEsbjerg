fff40af798564a880d59dc7cbeabe8354407f212
DanielPincu <ghepin01@easv365.dk>
2024-05-24 18:22:45 +0200

    Footer, banner, updated

--- src/components/Footer-section.vue
+++ src/components/Footer-section.vue
@@ -1,6 +1,6 @@
 <template>
      <footer class="w-full bg-[#1A202C] flex flex-col items-center justify-center text-center">
-      <img src="../assets/img/footer.webp" alt="">
+      <img src="../assets/img/footer2.webp" alt="">
       <div class="bg-[#165678] w-full grid justify-center">
         <p class="text-xl">Sponsored by</p>
         <img src="../assets/img/sponsors.webp" alt="Festiva's sponsors">
